2964c37d8e5a7a2996672be9a8315351eb54bbb5
mosesglitch <mosesgachuru15@gmail.com>
2023-05-01 01:14:25 +0300

    add app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+function App() {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  c96e7bfa-5bbb-418f-97b5-a88cb15409f8"
+      userName="Mosee"
+      userSecret="123123"
+    />
+  );
+}
+
+export default App;
